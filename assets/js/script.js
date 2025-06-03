b0b23af7d9730e4ce3f6f9a90f93e6c416511612
Loan <lgiovaniloh@gmail.com>
2025-06-04 00:34:28 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ function stargame() {
     })
 
 }
-const musique = new Audio("assets/ressources/calmly-30-sec-edit-301145.mp3");
+const musique = new Audio("assets/ressources/room69.opus");
 musique.load();
 musique.loop = true;
 musique.volume = 0.2;
